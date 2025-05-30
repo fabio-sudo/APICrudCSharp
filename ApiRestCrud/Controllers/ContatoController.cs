3a87914e8fe9b69c61802acb5ef7f2b4acfcb744
Fábio-Sudo <fabiotrevezane@gmail.com>
2025-05-30 09:26:55 -0300

    Atualizado

--- ApiRestCrud/Controllers/ContatoController.cs
+++ ApiRestCrud/Controllers/ContatoController.cs
@@ -26,11 +26,62 @@ namespace ApiRestCrud.Controllers
             _context.Add(contato);       // Adiciona o objeto 'contato' à memória do DbContext.
             _context.SaveChanges();      // Salva de fato no banco (executa o INSERT).
 
-            return Ok(contato);          // Retorna status 200 (OK) com o contato inserido como resposta.
+           // return Ok(contato);          // Retorna status 200 (OK) com o contato inserido como resposta.
+           return CreatedAtAction(nameof(ObterPorId),new {id = contato.Id}, contato);//retorna rota para buscar contato add
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id,Contato contato) {
+
+            var contatoBanco = _context.Contato.Find(id);
+
+            if (contatoBanco == null) 
+                return NotFound(); 
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Ativo = contato.Ativo;
+            contatoBanco.Telefone = contato.Telefone;
+
+            _context.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var contatoBanco = _context.Contato.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            _context.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            //return Ok(contatoBanco);
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contato.Find(id);
+
+            if (contato == null)
+                return NotFound();
 
 
+            return Ok(contato);
+        }
+
+        [HttpGet("ObterPorNome/{nome}")]
+        public IActionResult BuscarPorNome(string nome)
+        {
+            var contatos = _context.Contato.Where(x=> x.Nome.Contains(nome)).ToList();  
+            return Ok(contatos);
+        }
         #endregion
 
 
